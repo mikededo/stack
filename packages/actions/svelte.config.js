d2a6836dbad2f06183f26c72c671a652bc797ea1
Miquel de Domingo <miquelddg@gmail.com>
2024-07-25 12:02:36 +0200

    fix(packages/actions): correct generated output

--- packages/actions/svelte.config.js
+++ packages/actions/svelte.config.js
@@ -3,7 +3,6 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: ['.svelte', '.ts'],
   preprocess: [vitePreprocess()],
   vitePlugin: { inspector: true }
 };
