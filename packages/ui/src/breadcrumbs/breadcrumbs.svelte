1b5f2c07c87e7710074527927fa21c156af5514f
Miquel de Domingo <miquelddg@gmail.com>
2024-07-30 18:37:56 +0200

    feat(packages/ui): added `Breadcrumbs` component

---
+++ packages/ui/src/breadcrumbs/breadcrumbs.svelte
@@ -0,0 +1,31 @@
+<script lang="ts" context="module">
+    type Crumb = { label: string; href: string };
+    export type Crumbs = [Crumb, ...Crumb[], Omit<Crumb, 'href'>];
+</script>
+
+<script lang="ts">
+    type Props = { breadcrumbs: Crumbs };
+    let { breadcrumbs }: Props = $props();
+</script>
+
+<nav aria-label="Breadcrumbs">
+    <ol class="ui-flex ui-flex-row ui-gap-1 ui-text-sm ui-text-secondary-300 ui-duration-75">
+        {#each breadcrumbs as crumb, i (i)}
+            {#if crumb.href}
+                <li>
+                    <a
+                        href={crumb.href}
+                        class="ui-transition-all hover:ui-text-foreground hover:ui-underline"
+                    >
+                        {crumb.label}
+                    </a>
+                </li>
+                <li aria-hidden="true">/</li>
+            {:else}
+                <li class="ui-cursor-default ui-text-foreground">
+                    {crumb.label}
+                </li>
+            {/if}
+        {/each}
+    </ol>
+</nav>
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,4 @@
 export { Button } from './button/index.js';
+export { Breadcrumbs, type Crumbs } from './breadcrumbs/index.js';
 export { ContextMenu, createContextMenu, type ContextMenuOption } from './context-menu/index.js';
 export { Input } from './input/index.js';
