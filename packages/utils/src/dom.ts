8fad0cf4b44c592df0990b749192dd945848822b
Miquel de Domingo <miquelddg@gmail.com>
2024-08-01 21:05:09 +0200

    feat(packages/utils): added keyboard and dom utils

---
+++ packages/utils/src/dom.ts
@@ -0,0 +1,22 @@
+// Credit:
+//  - https://stackoverflow.com/a/30753870
+const focusableSelector = [
+  '[contentEditable=true]',
+  '[tabindex]',
+  'a[href]',
+  'area[href]',
+  'button:not([disabled])',
+  'iframe',
+  'input:not([disabled])',
+  'select:not([disabled])',
+  'textarea:not([disabled])'
+]
+  .map((selector) => `${selector}:not([tabindex='-1'])`)
+  .join(',');
+
+export const getFocusableElements = (
+  container: HTMLElement | null = document.body
+) =>
+  container == null
+    ? []
+    : Array.from(container.querySelectorAll<HTMLElement>(focusableSelector));
---
+++ packages/utils/src/keyboard.ts
@@ -0,0 +1,8 @@
+export const Keys = {
+  Enter: 'Enter',
+  Escape: 'Escape',
+  ArrowLeft: 'ArrowLeft',
+  ArrowUp: 'ArrowUp',
+  ArrowRight: 'ArrowRight',
+  ArrowDown: 'ArrowDown'
+};
