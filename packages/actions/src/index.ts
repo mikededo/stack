1e9a71e5568fcbe25f41fac38bd6bd63d39656de
Miquel de Domingo <miquelddg@gmail.com>
2024-08-01 21:05:51 +0200

    feat(packages/actions): add `use-actions` hook

--- packages/actions/src/index.ts
+++ packages/actions/src/index.ts
@@ -1,2 +1,4 @@
+export { useActions } from './use-actions.js';
+export type { ActionArray } from './use-actions.js';
 export { clickAway } from './click-away.js';
 export { portal } from './portal.js';
---
+++ packages/actions/src/use-actions.ts
@@ -0,0 +1,56 @@
+// This is a modified version of hperrin/svelte-material-ui
+// Copyright 2020-present Hunter Perrin
+type SvelteActionReturnType<P> = {
+  update?: (newParams?: P) => void;
+  destroy?: () => void;
+} | void;
+type SvelteHTMLActionType<P> = (node: HTMLElement, params?: P) => SvelteActionReturnType<P>;
+type HTMLActionEntry<P extends any = any> = SvelteHTMLActionType<P> | [SvelteHTMLActionType<P>, P];
+export type ActionArray = HTMLActionEntry[];
+
+export function useActions(node: HTMLElement, actions: ActionArray) {
+  let actionReturns: SvelteActionReturnType<any>[] = [];
+
+  if (actions) {
+    for (let i = 0; i < actions.length; i++) {
+      const actionEntry = actions[i];
+      const action = Array.isArray(actionEntry) ? actionEntry[0] : actionEntry;
+      if (Array.isArray(actionEntry) && actionEntry.length > 1) {
+        actionReturns.push(action(node as HTMLElement, actionEntry[1]));
+      } else {
+        actionReturns.push(action(node as HTMLElement));
+      }
+    }
+  }
+
+  return {
+    update(actions: ActionArray) {
+      if (((actions && actions.length) || 0) != actionReturns.length) {
+        throw new Error('You must not change the length of an actions array.');
+      }
+
+      if (actions) {
+        for (let i = 0; i < actions.length; i++) {
+          const returnEntry = actionReturns[i];
+          if (returnEntry && returnEntry.update) {
+            const actionEntry = actions[i];
+            if (Array.isArray(actionEntry) && actionEntry.length > 1) {
+              returnEntry.update(actionEntry[1]);
+            } else {
+              returnEntry.update();
+            }
+          }
+        }
+      }
+    },
+
+    destroy() {
+      for (let i = 0; i < actionReturns.length; i++) {
+        const returnEntry = actionReturns[i];
+        if (returnEntry && returnEntry.destroy) {
+          returnEntry.destroy();
+        }
+      }
+    }
+  };
+}
