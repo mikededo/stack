0952c52051e5f7a39a4745d85d98011604e42cfb
Miquel de Domingo <miquelddg@gmail.com>
2024-08-01 11:26:23 +0200

    fix(packages/actions): forward event on `clickAway`

--- packages/actions/src/click-away.ts
+++ packages/actions/src/click-away.ts
@@ -1,15 +1,21 @@
 import type { Action } from 'svelte/action';
 
-type Handler = () => void | undefined;
+type ClickAwayHandler = ((e: MouseEvent) => void) | undefined;
 
-export const clickAway: Action<HTMLElement, Handler> = (node, handler) => {
+export const clickAway: Action<HTMLElement, ClickAwayHandler> = (node, handler) => {
   const onClick = (event: MouseEvent) => {
+    if (typeof handler !== 'function') {
+      // Added to ensure TS
+      return;
+    }
+
     if (!node.contains(event.target as HTMLElement) && !event.defaultPrevented) {
-      handler?.();
+      handler(event);
     }
   };
 
   document.addEventListener('click', onClick, true);
+
   return {
     destroy() {
       document.removeEventListener('click', onClick, true);
