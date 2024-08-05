854a3e76a839f81d01628a80cab13e3938845953
Miquel de Domingo <miquelddg@gmail.com>
2024-08-05 14:34:19 +0200

    feat(packages/utils): add regex for hex colors

--- packages/utils/src/regex.ts
+++ packages/utils/src/regex.ts
@@ -1,3 +1,5 @@
 export const NAME_REGEX = /^[a-zA-Z\s]+$/;
 export const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 export const PASSWORD_REGEX = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}/;
+
+export const HEX_REGEX = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
