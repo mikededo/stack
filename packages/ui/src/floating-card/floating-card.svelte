a96c911849d8e46450d021f9e61a63912e67bae3
Miquel de Domingo <miquelddg@gmail.com>
2024-08-07 20:48:51 +0200

    fix(packages/ui): minor `FloatingCard` improvements

--- packages/ui/src/floating-card/floating-card.svelte
+++ packages/ui/src/floating-card/floating-card.svelte
@@ -9,15 +9,22 @@
     import { twMerge } from 'tailwind-merge';
 
     type Props = HTMLAttributes<HTMLDivElement> & {
+        position?: { top: number; left: number; width: number };
         children: Snippet;
         onClickAway?: Parameters<typeof clickAway>[1];
         use?: ActionArray;
     };
-    let { children, onClickAway, use = [], ...restProps }: Props = $props();
+    let { children, position, onClickAway, use = [], ...restProps }: Props = $props();
 
+    let positionStyle = $derived(
+        position
+            ? `top: ${position.top}px; left: ${position.left}px; width: ${position.width}px;`
+            : undefined
+    );
     let classes = $derived(
         twMerge(
-            'ui-absolute -ui-bottom-2 ui-right-0 ui-z-10 ui-flex ui-w-full ui-min-w-48 ui-origin-top-right ui-translate-y-full ui-flex-col ui-items-start ui-gap-[1px] ui-rounded-lg ui-border ui-border-secondary-100 ui-bg-white ui-p-1 ui-shadow-lg',
+            'ui-z-10 ui-flex ui-w-full ui-min-w-48 ui-origin-top-right ui-flex-col ui-items-start ui-gap-[1px] ui-rounded-lg ui-border ui-border-secondary-100 ui-bg-white ui-p-1 ui-shadow-lg',
+            position ? 'ui-fixed' : 'ui-absolute -ui-bottom-2 ui-right-0 ui-translate-y-full',
             restProps.class
         )
     );
@@ -25,6 +32,7 @@
 
 <div
     {...restProps}
+    style={positionStyle}
     class={classes}
     transition:fade={{ duration: 150, easing: cubicInOut }}
     use:useActions={use}
