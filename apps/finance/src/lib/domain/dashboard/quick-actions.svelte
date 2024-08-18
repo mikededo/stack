3b36d17971db3f2e533576269544b9b25fdf295b
Miquel de Domingo <miquelddg@gmail.com>
2024-08-18 14:41:36 +0200

    feat(apps/finance): disable quick action if not available

--- apps/finance/src/lib/domain/dashboard/quick-actions.svelte
+++ apps/finance/src/lib/domain/dashboard/quick-actions.svelte
@@ -2,6 +2,9 @@
     import { BookPlus, FilePlus, Pencil, Plus } from 'lucide-svelte';
 
     import { showDialog } from '$lib/domain/dialog';
+    import { useLastViewedPages } from '$lib/hooks';
+
+    const lastViewedPages = useLastViewedPages();
 
     const onNewBookClick = () => {
         showDialog('CREATE_BOOK');
@@ -10,29 +13,36 @@
     const Actions = [
         { icon: BookPlus, name: 'Add a new book', onClick: onNewBookClick },
         { icon: FilePlus, name: 'Add a new page' },
-        { icon: Pencil, name: 'Open last edited page' }
+        {
+            disabled: !$lastViewedPages.data?.length,
+            icon: Pencil,
+            loading: $lastViewedPages.isLoading,
+            name: 'Open last edited page'
+        }
     ];
 </script>
 
 <section class="flex flex-col gap-3 md:mb-4">
     <p class="text-sm font-semibold">Quick actions</p>
     <div class="grid grid-cols-2 gap-2 lg:grid-cols-3 xl:grid-cols-4">
-        {#each Actions as { icon, name, onClick }}
+        {#each Actions as { disabled, icon, loading, name, onClick }}
             <button
-                class="group flex w-full flex-col gap-3 rounded-lg border border-surface-200 bg-white p-3 transition-all duration-150 hover:border-primary-300"
+                class="group flex w-full flex-col gap-3 rounded-lg border border-surface-200 bg-white p-3 transition-all duration-150 hover:border-primary-300 disabled:cursor-not-allowed disabled:hover:border-surface-200"
+                disabled={disabled || loading}
+                class:animate-pulse={loading}
                 onclick={onClick}
             >
                 <div class="flex w-full items-start justify-between">
                     <div
-                        class="flex size-10 items-center justify-center rounded-lg bg-primary-500 p-2"
+                        class="flex size-10 items-center justify-center rounded-lg bg-primary-500 p-2 group-disabled:bg-surface-200"
                     >
                         <svelte:component this={icon} class="stroke-white" />
                     </div>
                     <Plus
-                        class="size-4 stroke-surface-700 transition-all group-hover:rotate-90 group-hover:stroke-primary"
+                        class="size-4 stroke-surface-700 transition-all group-hover:rotate-90 group-hover:stroke-primary group-disabled:rotate-0 group-disabled:stroke-surface-700"
                     />
                 </div>
-                <span class="text-sm font-semibold">{name}</span>
+                <span class="text-sm font-semibold group-disabled:text-surface-700">{name}</span>
             </button>
         {/each}
     </div>
