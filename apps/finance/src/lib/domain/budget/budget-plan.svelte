6fa22983a68215d77b6da27ce491851562a023c9
Miquel de Domingo <miquelddg@gmail.com>
2024-08-24 09:20:57 +0200

    feat(apps/finance): append created budget plan to cached data

--- apps/finance/src/lib/domain/budget/budget-plan.svelte
+++ apps/finance/src/lib/domain/budget/budget-plan.svelte
@@ -1,10 +1,15 @@
 <!-- Specific plan render for the saved plans -->
 <script lang="ts">
+    import { fade } from 'svelte/transition';
+
     import type { BudgetPlan } from '$lib/db';
+
+    import { beforeNavigate } from '$app/navigation';
 
     type Props = { plan: BudgetPlan };
     let { plan }: Props = $props();
     let { allocations, name, total_income } = plan;
+    let duration = $state(100);
 
     const getAllocationWidth = (
         percentage: null | number,
@@ -17,10 +22,15 @@
 
         return percentage ? percentage : (amount! / budget) * 100;
     };
+
+    beforeNavigate(() => {
+        duration = 0;
+    });
 </script>
 
 <button
     class="group w-full rounded border border-surface-200 p-3 text-left transition-all hover:border-solid hover:border-primary"
+    transition:fade|global={{ duration }}
 >
     <p class="truncate text-lg font-semibold">{name}</p>
     <p class="mb-0.5">€ {total_income}</p>
