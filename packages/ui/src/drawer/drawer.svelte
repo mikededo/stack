62900a22985e8cdce76f52875be5f4649315c08f
Miquel de Domingo <miquelddg@gmail.com>
2024-08-24 13:03:25 +0200

    fix(packages/ui): better drawer animations

--- packages/ui/src/drawer/drawer.svelte
+++ packages/ui/src/drawer/drawer.svelte
@@ -12,6 +12,12 @@
 
     import { IconButton } from '../icon-button/index.js';
 
+    const DRAWER_WIDTH = 480;
+    const MAX_DRAWER_POSITION = 488;
+    const MIN_DRAWER_POSITION = 0;
+    const MIN_OVERLAY_OPACITY = 0;
+    const MAX_OVERLAY_OPACITY = 1;
+
     type Props = {
         children: Snippet;
         header: string;
@@ -22,8 +28,8 @@
     };
     let { children, header, Icon, onClose, show, subtitle }: Props = $props();
 
-    let internalShow = $state(show ?? false);
-    const position = spring(0, { damping: 1, precision: 1, stiffness: 0.3 });
+    const internalShow = spring(show ? MAX_OVERLAY_OPACITY : MIN_OVERLAY_OPACITY);
+    const position = spring(show ? MAX_DRAWER_POSITION : MIN_DRAWER_POSITION);
 
     let contentElement: HTMLElement | null = null;
     position.subscribe((value) => {
@@ -37,12 +43,12 @@
             return;
         }
 
-        const scale = ((value - 0) * (2 - 0)) / (480 - 0) + 0;
+        const scale = ((value - 0) * (2 - 0)) / (DRAWER_WIDTH - 0) + 0;
         contentElement.style.transform = `scale(${1 - scale / 100})`;
     });
 
     const onInternalClose = () => {
-        position.set(0);
+        position.set(MIN_DRAWER_POSITION);
         onClose?.();
     };
 
@@ -66,29 +72,29 @@
 
     $effect(() => {
         if (show) {
-            internalShow = true;
-            position.set(488);
+            internalShow.set(MAX_OVERLAY_OPACITY);
+            position.set(MAX_DRAWER_POSITION);
         } else {
             setTimeout(() => {
-                internalShow = false;
-            }, 250);
-            position.set(0);
+                internalShow.set(MIN_OVERLAY_OPACITY);
+            }, 150);
+            position.set(MIN_DRAWER_POSITION);
         }
     });
 </script>
 
-{#if internalShow}
+{#if $internalShow}
     <div
         class="ui-fixed ui-inset-0 ui-bg-black/30"
         use:useActions={[[portal, ''], useOnClick]}
-        style="opacity: {$position / 480}"
+        style="opacity: {$internalShow}"
     ></div>
 {/if}
 <div
     class="ui-fixed ui-bottom-2 ui-top-2 ui-z-10 ui-flex ui-overflow-x-hidden"
     use:portal={''}
     role="dialog"
-    style="right: {-480 + $position}px"
+    style="right: {-DRAWER_WIDTH + $position}px"
 >
     <div
         class="ui-flex ui-h-full ui-w-[480px] ui-flex-col ui-items-center ui-gap-6 ui-rounded-lg ui-bg-white ui-px-5 ui-py-4"
