aebe6bb1dbd6c5842806b21ebd1f22783477ca58
Miquel de Domingo <miquelddg@gmail.com>
2024-09-11 09:14:18 +0200

    feat(packages/actions): improved `click-away` action

--- packages/actions/src/click-away.ts
+++ packages/actions/src/click-away.ts
@@ -1,16 +1,40 @@
 import type { Action } from 'svelte/action';
 
-type ClickAwayHandler = ((e: MouseEvent) => void) | undefined;
+type ClickAwayHandler = (event: MouseEvent) => void;
+type ClickAwayOptions = {
+  /**
+   * A callback that receives the event and should return `true`
+   * if the click away should be executed
+   */
+  shouldClickAway?: (event: MouseEvent) => boolean;
+};
 
-export const clickAway: Action<HTMLElement, ClickAwayHandler> = (node, handler) => {
-  const onClick = (event: MouseEvent) => {
-    if (typeof handler !== 'function') {
-      // Added to ensure TS
-      return;
-    }
+type HandlerWithOptions = [ClickAwayHandler, ClickAwayOptions?];
 
-    if (!node.contains(event.target as HTMLElement) && !event.defaultPrevented) {
+export const clickAway: Action<HTMLElement, ClickAwayHandler | HandlerWithOptions> = (
+  node,
+  handler
+) => {
+  const isClickAway = (event: MouseEvent) =>
+    !node.contains(event.target as HTMLElement) && !event.defaultPrevented;
+
+  const executeHandler = (
+    event: MouseEvent,
+    handler: ClickAwayHandler,
+    options?: ClickAwayOptions
+  ) => {
+    const { shouldClickAway } = options || {};
+    if (isClickAway(event) && (!shouldClickAway || shouldClickAway(event))) {
       handler(event);
+    }
+  };
+
+  const onClick = (event: MouseEvent) => {
+    if (typeof handler === 'function') {
+      executeHandler(event, handler as ClickAwayHandler);
+    } else if (Array.isArray(handler)) {
+      const [callback, options] = handler;
+      executeHandler(event, callback, options);
     }
   };
 
