b187d0629dc262388034d06a4fc3880ee0f27bcd
Miquel de Domingo <miquelddg@gmail.com>
2024-09-20 10:51:13 +0200

    fix(packages/actions): correct `shift-tab` on `useTrapFocus`

--- packages/actions/src/use-trap-focus.ts
+++ packages/actions/src/use-trap-focus.ts
@@ -7,11 +7,12 @@ export const useTrapFocus: Action = (node) => {
     const focusableElements = getFocusableElements(node);
     const iof = focusableElements.indexOf(document.activeElement as HTMLElement);
 
-    if (e.key === Keys.ArrowDown || e.key === Keys.Tab) {
+    if (e.key === Keys.ArrowDown || (e.key === Keys.Tab && !e.shiftKey)) {
       e.preventDefault();
       const nextIndex = (iof + 1) % focusableElements.length;
       focusableElements[nextIndex].focus();
     } else if (e.key === Keys.ArrowUp || (e.key === Keys.Tab && e.shiftKey)) {
+      e.preventDefault();
       const prevIndex = (iof - 1 + focusableElements.length) % focusableElements.length;
       focusableElements[prevIndex].focus();
     }
