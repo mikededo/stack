6578a5be3c91a13cf82d2e33e1a93229aeef1399
Miquel de Domingo <miquelddg@gmail.com>
2024-09-20 12:40:25 +0200

    feat(packages/utils): added keys

--- packages/utils/src/keyboard.ts
+++ packages/utils/src/keyboard.ts
@@ -4,7 +4,11 @@ export const Keys = {
   ArrowRight: 'ArrowRight',
   ArrowUp: 'ArrowUp',
   Backspace: 'Backspace',
+  End: 'End',
   Enter: 'Enter',
   Escape: 'Escape',
+  Home: 'Home',
+  PageDown: 'PageDown',
+  PageUp: 'PageUp',
   Tab: 'Tab'
 };
