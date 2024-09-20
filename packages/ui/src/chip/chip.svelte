f786e5b6279a6846848e63bdb33822fd47c00d0d
Miquel de Domingo <miquelddg@gmail.com>
2024-09-20 10:51:38 +0200

    feat(packages/ui): add clickable chip

--- packages/ui/src/chip/chip.svelte
+++ packages/ui/src/chip/chip.svelte
@@ -1,27 +1,38 @@
 <script lang="ts">
     import type { Snippet } from 'svelte';
-    import type { SvelteHTMLElements } from 'svelte/elements';
 
+    import { X } from 'lucide-svelte';
     import { twMerge } from 'tailwind-merge';
 
     type ChipVariant = 'primary';
 
     type BaseProps = {
         children: Snippet;
+        class?: string;
+        onClick?: () => void;
     } & ({ color: string; variant?: never } | { variant: ChipVariant; color?: never });
-    type Props = BaseProps & SvelteHTMLElements['span'];
-    let { children, color, variant, ...restProps }: Props = $props();
+    type Props = BaseProps;
+    let { children, color, onClick, variant, ...restProps }: Props = $props();
 
     const classes = $derived(
         twMerge(
-            'ui-rounded-full ui-px-2 ui-py-1 ui-text-xs ui-font-semibold',
+            'ui-rounded-full ui-bg-current ui-px-2 ui-py-1 ui-text-xs ui-font-semibold',
             variant === 'primary' && 'ui-bg-primary/25 ui-text-primary',
+            onClick &&
+                'ui-flex ui-items-center ui-gap-1 ui-outline-none ui-transition-all hover:ui-opacity-90 focus:ui-ring-2 focus:ui-ring-current focus-visible:ui-scale-[0.975] focus-visible:ui-outline-none active:ui-scale-[0.975] disabled:ui-cursor-not-allowed disabled:active:ui-scale-100',
             restProps.class
         )
     );
     const style = $derived(variant ? undefined : `background-color: ${color}44; color: ${color}`);
 </script>
 
-<span {...restProps} class={classes} {style}>
-    {@render children()}
-</span>
+{#if onClick}
+    <button {...restProps} class={classes} {style} onclick={onClick}>
+        {@render children()}
+        <X size={14} strokeWidth={2.5} />
+    </button>
+{:else}
+    <span {...restProps} class={classes} {style}>
+        {@render children()}
+    </span>
+{/if}
