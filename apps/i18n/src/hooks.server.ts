5a3d8f551ce46b406cbbb7dc17024140c6132a1f
Miquel de Domingo <miquelddg@gmail.com>
2024-10-06 17:49:46 +0200

    feat(apps/i18n): app starter

---
+++ apps/i18n/src/hooks.server.ts
@@ -0,0 +1,29 @@
+import { createSupabaseServerClient, getSession } from '@stack/supabase';
+
+import { type Handle, redirect } from '@sveltejs/kit';
+
+import { getEnv, pathTo } from '$lib/config';
+
+// Since we are running the production build with bun, we have to use
+// procees.env to get the env variables
+
+export const handle: Handle = async ({ event, resolve }) => {
+  const { supabaseAnonKey, supabaseUrl } = getEnv();
+
+  event.locals.supabase = createSupabaseServerClient(
+    supabaseUrl,
+    supabaseAnonKey,
+    event
+  );
+  event.locals.safeGetSession = async () => getSession(event.locals.supabase);
+
+  if (event.url.pathname === '/') {
+    throw redirect(302, pathTo('app'));
+  }
+
+  return resolve(event, {
+    filterSerializedResponseHeaders(name) {
+      return name === 'content-range' || name === 'x-supabase-api-version';
+    }
+  });
+};
---
+++ apps/i18n/src/lib/config/routes.ts
@@ -0,0 +1,46 @@
+import { get } from 'svelte/store';
+
+import { page } from '$app/stores';
+
+type Route = {
+  app: undefined;
+  auth: undefined;
+  // Fill this
+};
+
+type Routes = keyof Route;
+
+const Paths: Record<Routes, string> = {
+  app: '/app',
+  auth: '/auth'
+};
+
+export const isCurrentPath = (path: string | undefined): boolean => get(page).url.pathname === path;
+
+export const isNestedPath = (path: string | undefined, nested: string): boolean => {
+  if (!path) {
+    return false;
+  }
+
+  if (Object.keys(Paths).includes(nested)) {
+    const routePattern = Paths[nested as Routes];
+    const regexPattern = new RegExp(`^${routePattern.replace(/:[^/]+/g, '[^/]+')}$`);
+    return regexPattern.test(path);
+  }
+
+  return false;
+};
+
+export const pathTo = <T extends Routes>(route: T, params?: Route[T]): string => {
+  if (params !== undefined) {
+    return Object.keys(params).reduce(
+      (path, key) => path.replace(`:${key}`, (params as Record<string, string>)[key]),
+      Paths[route]
+    );
+  } else {
+    return Paths[route];
+  }
+};
+
+export const isAuthPath = (path: string | undefined) => path === Paths.auth;
+export const isAuthRelated = (path: string | undefined) => path?.startsWith(Paths.auth);
