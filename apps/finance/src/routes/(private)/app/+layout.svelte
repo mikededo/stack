8a581989b0e28b37573d37c21379546ff7bfeb7f
Miquel de Domingo <miquelddg@gmail.com>
2024-10-13 08:42:26 +0200

    feat(apps/finance): use shared layout

--- apps/finance/src/routes/(private)/app/+layout.svelte
+++ apps/finance/src/routes/(private)/app/+layout.svelte
@@ -1,22 +1,19 @@
 <script lang="ts">
     import type { ComponentType, Snippet } from 'svelte';
 
+    import { Content01 } from '@stack/layouts';
     import { setUserDataContext } from '@stack/supabase';
-    import { Logo } from '@stack/ui';
 
     import type { LayoutData } from './$types';
 
     import {
         DollarSign,
-        Ellipsis,
         LayoutDashboard,
         Icon as LucideIcon,
         PiggyBank
     } from 'lucide-svelte';
-    import { fade } from 'svelte/transition';
 
-    import { page } from '$app/stores';
-    import { isNestedPath, pathTo } from '$lib/config';
+    import { pathTo } from '$lib/config';
 
     type Props = { children: Snippet; data: LayoutData };
     const { children, data }: Props = $props();
@@ -31,68 +28,10 @@
     ];
 </script>
 
-<header class="hidden h-12 w-full bg-surface-50 md:flex">
-    <div class="hidden h-full items-center gap-2 bg-transparent px-4 md:flex md:min-w-64">
-        <Logo />
-        <p class="font-bold">Stack</p>
-    </div>
-    <div class="flex h-full w-full items-center justify-end pr-5">
-        <!-- TODO: Add user preferences -->
-        <span
-            class="cursor-pointer text-sm text-surface-700 underline decoration-transparent underline-offset-1 transition-colors hover:decoration-surface-700"
-        >
-            Welcome back, {data.user.first_name}!
-        </span>
-    </div>
-</header>
+<Content01 pathname={data.pathname}
+    tabs={tabs}
+    userName={data.user.first_name}
+>
+    {@render children()}
+</Content01>
 
-<div class="flex w-full flex-col overflow-hidden bg-background bg-surface-50 md:flex-row">
-    <!-- Desktop navigation -->
-    <div class="hidden h-content-md overflow-x-hidden px-2 md:block md:min-w-64">
-        <nav class="h-full gap-1 py-10">
-            <ul class="flex w-full flex-col gap-1">
-                {#each tabs as { disabled, href, Icon, name } (href)}
-                    <li>
-                        <a
-                            class="flex w-full cursor-pointer items-center justify-between rounded px-3 py-2 text-sm font-semibold transition-colors aria-disabled:pointer-events-none aria-disabled:text-surface-400 aria-current:bg-primary-100 aria-current:text-primary aria-not-current:hover:bg-primary-100"
-                            aria-current={href === $page.url.pathname || isNestedPath(href, $page.url.pathname)}
-                            aria-disabled={disabled}
-                            role="tab"
-                            {href}
-                        >
-                            <span>{name}</span>
-                            <svelte:component this={Icon} class="size-4" strokeWidth={2} />
-                        </a>
-                    </li>
-                {/each}
-            </ul>
-        </nav>
-    </div>
-
-    <!-- Mobile navigation -->
-    <header class="block bg-surface-50 md:hidden">
-        <nav class="flex h-12 items-center justify-between px-2">
-            <div class="flex items-center gap-2">
-                <Logo />
-                <p class="font-bold">Stack</p>
-            </div>
-            <Ellipsis class="size-4" strokeWidth={2.5} />
-        </nav>
-    </header>
-
-    <div class="h-full w-full px-2 pb-2 pt-0 md:pb-4 md:pl-4">
-        <main
-            class="w-full flex-1 overflow-hidden rounded-lg border border-surface-200 bg-white scrollbar-thin md:w-content-md lg:scrollbar"
-        >
-            {#key data.pathname}
-                <div
-                    class="flex h-content flex-col gap-4 overflow-y-auto p-6 md:h-content-md md:px-10 md:py-8"
-                    in:fade={{ delay: 100, duration: 100 }}
-                    out:fade={{ duration: 100 }}
-                >
-                    {@render children()}
-                </div>
-            {/key}
-        </main>
-    </div>
-</div>
--- apps/finance/src/routes/+page.svelte
+++
@@ -1,5 +0,0 @@
-<h1>Welcome to SvelteKit</h1>
-
-<p>
-    Visit <a class="hover:underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
