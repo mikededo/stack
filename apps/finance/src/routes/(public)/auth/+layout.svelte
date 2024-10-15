0512c4e0009d81642ec9e4526300ebea6232ea46
Miquel de Domingo <miquelddg@gmail.com>
2024-10-15 17:47:38 +0200

    refactor(apps/finance): use extracted layout

--- apps/finance/src/routes/(public)/auth/+layout.svelte
+++ apps/finance/src/routes/(public)/auth/+layout.svelte
@@ -1,38 +1,10 @@
 <script lang="ts">
     import type { Snippet } from 'svelte';
 
+    import { Auth } from '@stack/layouts';
+
     type Props = { children: Snippet };
     const { children }: Props = $props();
 </script>
 
-<svelte:head>
-    <title>Authentication | Finance · stack</title>
-    <meta content="Sign in or sign up to access your Finance account" name="description" />
-    <meta content="noindex, nofollow" name="robots" />
-    <meta content="#ffffff" name="theme-color" />
-    <meta content="width=device-width, initial-scale=1" name="viewport" />
-    <meta content="Lang Hub" name="apple-mobile-web-app-title" />
-    <meta content="Lang Hub" name="application-name" />
-    <meta content="website" property="og:type" />
-    <meta content="TBD" property="og:url" />
-    <meta content="Authentication | Finance · stack" property="og:title" />
-    <meta content="Sign in or sign up to access your Finance account" property="og:description" />
-    <meta content="TBD" property="og:image" />
-    <meta content="1200" property="og:image:width" />
-    <meta content="630" property="og:image:height" />
-</svelte:head>
-
-<main>
-    <div class="fixed left-0 top-0 h-full w-full bg-background">
-        <div
-            class="absolute inset-0 h-full w-full bg-[radial-gradient(#e7e7e7_1px,transparent_1px)] [background-size:16px_16px] dark:bg-[radial-gradient(#333333_1px,transparent_1px)]"
-        ></div>
-    </div>
-    <div class="flex min-h-screen items-center">
-        <div
-            class="fixed inset-0 overflow-auto bg-background p-8 md:relative md:mx-auto md:w-full md:max-w-md md:rounded md:border"
-        >
-            {@render children()}
-        </div>
-    </div>
-</main>
+<Auth appName="Finance" {children} />
