246388172f33b51d7a215bc758f4f9b6141ed2e4
Miquel de Domingo <miquelddg@gmail.com>
2024-10-16 11:17:56 +0200

    feat(packages/utils): add object prop transformer type

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -11,3 +11,17 @@ export type MutationResult<
     Context
   >
 >;
+
+export type CamelCase<S extends string> = S extends `${infer T}_${infer U}`
+  ? `${T}${Capitalize<CamelCase<U>>}`
+  : S;
+export type TrimPrefix<S extends string, Prefix extends string> = S extends `${Prefix}${infer Rest}`
+  ? Rest
+  : S;
+export type CamelCaseProperties<T, Prefix extends string = ''> = T extends object
+  ? T extends Array<infer U>
+    ? Array<CamelCaseProperties<U, Prefix>>
+    : {
+        [K in keyof T as CamelCase<TrimPrefix<K & string, Prefix>>]: CamelCaseProperties<T[K], Prefix>;
+      }
+  : T;
