0ca5a76727b4e6304b681dbcdef5a6e137f809f4
Miquel de Domingo <miquelddg@gmail.com>
2024-11-09 11:02:57 +0100

    feat(packages/ui): add textarea to `Input`

--- packages/ui/src/input/input.svelte
+++ packages/ui/src/input/input.svelte
@@ -1,27 +1,35 @@
 <script lang="ts">
     import type { ActionArray } from '@stack/actions';
 
-    import type { HTMLInputAttributes } from 'svelte/elements';
+    import type { HTMLInputAttributes, HTMLTextareaAttributes } from 'svelte/elements';
 
     import { useActions } from '@stack/actions';
 
     import { twMerge } from 'tailwind-merge';
 
     type InputColor = 'destructive' | 'primary' | 'surface';
-
-    type Props = {
+    type BaseProps = {
         color?: InputColor;
         invalid?: boolean;
         label?: string;
-        ref?: HTMLInputElement;
         use?: ActionArray;
+    };
+    type InputProps = {
+        multiline?: never;
+        ref?: HTMLInputElement;
     } & HTMLInputAttributes;
+    type TextAreaProps = {
+        multiline: true;
+        ref?: HTMLTextAreaElement;
+    } & HTMLTextareaAttributes;
+    type Props = BaseProps & (InputProps | TextAreaProps);
+
     let {
         color,
         invalid,
         label,
         name,
-        ref = $bindable<HTMLInputElement>(),
+        ref = $bindable<HTMLInputElement | HTMLTextAreaElement>(),
         use = [],
         value = $bindable<string>(''),
         ...rest
@@ -29,7 +37,7 @@
 
     const COLORS: Record<InputColor, string> = {
         destructive: 'ui-bg-destructive-50 ui-border-destructive-500 ui-text-destructive-500',
-        primary: 'focus:ui-border-primary active:ui-border-primary input',
+        primary: 'hover:ui-border-primary focus:ui-border-primary active:ui-border-primary input',
         surface:
             'ui-bg-surface-50 ui-border-transparent hover:ui-border-surface-200 focus:ui-border-surface-200 active:ui-border-surface-200 disabled:ui-bg-surface-100'
     };
@@ -37,23 +45,36 @@
     const classes = $derived(
         twMerge(
             COLORS[color ?? 'primary'],
-            'ui-h-10 ui-rounded ui-border ui-px-3 ui-outline-none ui-transition-colors disabled:ui-cursor-not-allowed disabled:ui-border-border',
+            'ui-h-10 ui-rounded ui-border ui-px-3 ui-outline-none ui-transition-all disabled:ui-cursor-not-allowed disabled:ui-border-border ui-shadow-none',
             invalid && 'hover:ui-border-desctructive ui-border-destructive focus:ui-border-destructive active:ui-border-destructive',
+            rest.multiline && 'ui-resize-none ui-scrollbar-thin ui-py-1.5',
             rest.class
         )
     );
 </script>
 
 {#snippet content()}
-    <input
-        {...rest}
-        class={classes}
-        bind:this={ref}
-        bind:value
-        use:useActions={use}
-        aria-invalid={invalid}
-        {name}
-    />
+    {#if rest.multiline}
+        <textarea
+            {...rest}
+            class={classes}
+            bind:this={ref}
+            bind:value
+            use:useActions={use}
+            aria-invalid={invalid}
+            {name}
+        ></textarea>
+    {:else}
+        <input
+            {...rest}
+            class={classes}
+            bind:this={ref}
+            bind:value
+            use:useActions={use}
+            aria-invalid={invalid}
+            {name}
+        />
+    {/if}
 {/snippet}
 
 {#if label}
